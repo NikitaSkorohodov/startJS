1965f2f0aa29125f52701f6fd685cf21bb7d48f2
NikitaSkorohodov <113201752+NikitaSkorohodov@users.noreply.github.com>
2023-09-04 14:44:25 +0300

    Add files via upload

---
+++ new.js
@@ -0,0 +1,7 @@
+'use strict'
+console.long("connected!")
+let number =10;
+const borderSize =1;
+console.log(number);
+number = 15;
+console.log(number);
---
+++ test.js
@@ -0,0 +1,21 @@
+//alert("hi");
+var a=10;
+var b=11;
+document.write("a="+a);
+if (b==11) {
+    var a=a+1;
+}
+document.write("a="+a);
+document.write("   b="+b);
+if (a==11) {
+    var b=b-1;
+}
+document.write("   b="+b);
+console.log("hi schooooool");
+document.write("<hr/>");
+var f= +prompt("введи температуру в фарингет");
+console.log;
+document.write("   f="+f);
+var f=Math.round((f-32)/1.8);
+document.write("   с="+f);
+alert("самой лучшей игрой можно назвать <<(введите свою игру)>>");
